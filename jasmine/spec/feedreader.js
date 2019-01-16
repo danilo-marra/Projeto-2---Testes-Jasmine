3dd0a3b94a75fabe1a6fa37f56021bad062829c4
Danilo Marra Rabelo <danilo2311@gmail.com>
2019-01-16 11:07:07 -0200

    atualização comentários
    sugestão feita pelos instrutores da Udacity

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,49 +1,25 @@
 /* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * Este é o arquivo spec que o Jasmine irá ler e contém todos os
+ * testes que irão verificar o sistema ao fim do carregamento da página
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+/* Todos os testes estão dentro do $() function, pois alguns testes necessitam utlizar elementos DOM.  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        /* Primeiro teste, que verifica se a variável allFeeds foi defininida e não está vazia */
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds).toBeDefined(); 
+            expect(allFeeds.length).not.toBe(0); 
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* Percorre cada elemento do array allFeeds para verificar se a propriedade URL está definida e não vazia */
         it('URL está definida e não vazia', function () {
            for (let feed of allFeeds) {
                 expect(feed.url).toBeDefined(); //URL defininida
                 expect(feed.url).not.toBeFalsy(); //URL não vazia
               }
         });
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-
+        /* Percorre cada elemento do array allFeeds para verificar se a propriedade NAME está definida e não vazia */
          it('name definido e não vazio', function () {
             for (let feed of allFeeds) {
                 expect(feed.name).toBeDefined(); //name definido
@@ -51,24 +27,14 @@ $(function() {
             }
          });
     });
-    /* TODO: Write a new test suite named "The menu" */
     describe('O menu', function() {
     
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        /* Teste que verifica se o menu está oculto por padrão. O nome da classe é 'menu-hidden' */
         it ('menu oculto', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
+        /* Altera a visibilidade do menu quando clicado, e altera novamente quando clicado pela segunda vez*/
         it('altera a visibilidade quando o menu é clicado', function () {
         $('.menu-icon-link').click();
         expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -77,36 +43,25 @@ $(function() {
 
         });
     });      
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Entradas iniciais', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        /* Teste que verifica quando a função loadFeed é invocada. Como ela á assincrona é necessário usar a função beforeEach do Jasmine*/
         beforeEach(function(done){
             loadFeed(0, done);
         });
         it('quando a função loadFeed for chamada e concluir seu trabalho, exista pelo menos um elemento .entry no contêiner .feed', function () {
-            expect($('.feed .entry').length).toBeGreaterThan(0);
+            expect($('.feed .entry').length).toBeGreaterThan(0); //Garantir que exista ao menos um elemento .entry no contêiner .feed
         });
     });
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('Nova seleção de feed', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-        const feeds = [];
-        beforeEach(function(done) {
+         //* Teste que checa o carregamento de um novo feed e compara com o feed anterior para fazer com que o conteúdo mude */
+        const feeds = []; //variável que vai armazenar os feeds
+        beforeEach(function(done) { 
              loadFeed(1, function() { //carrega o feed do elemento "1" e o armazena
                 feeds.push($('.feed').html());
                 loadFeed(2, done); //carrega o feed do elemento "2"
             });
-           
         });
-        it('quando um novo feed for carregado pela função loadFeed, o conteúdo realmente mude', function () {
+        it('alterar o conteúdo do feed quando um novo feed for carregado pela função loadFeed, ', function () {
             feeds.push($('.feed').html());
             expect(feeds[1]).not.toEqual(feeds[2]); //compara os 2 arrays
         });
